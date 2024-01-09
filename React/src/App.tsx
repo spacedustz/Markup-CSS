deb7c3e7cc04c6d1e89e2f7a6147d240db703f16
owen <floralexium@gmail.com>
2024-01-09 15:51:52 +0900

    패키지 이름 변경 & React Router 적용

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react'
-import TodayReport from "./components/TodayReport.tsx";
 import styled from "styled-components";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Day1 from "./components/Report/Day1.tsx";
+import Day2 from "./components/Report/Day2.tsx";
 
 const AppBlock = styled.div`
-  width: 512px;
-  margin: 0 auto;
-  margin-top: 4rem;
-  border: 1px solid black;
-  padding: 1rem;
+    width: 512px;
+    margin: 0 auto;
+    margin-top: 4rem;
+    border: 1px solid black;
+    padding: 1rem;
 `;
 
 const App: React.FC = () => {
 
     return (
-        <AppBlock>
-            <TodayReport />
-        </AppBlock>
+        <div>
+            <div>
+                <AppBlock>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/Day-01-Assignment" element={<Day1 />}/>
+                            <Route path="/Day-02-Assignment" element={<Day2 />}/>
+                        </Routes>
+                    </BrowserRouter>
+                </AppBlock>
+            </div>
 
+            <div>
+                <AppBlock>
 
-
+                </AppBlock>
+            </div>
+        </div>
     )
 }
 
--- React/src/components/TodayReport.tsx
+++ React/src/components/TodayReport.tsx
@@ -1,35 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import {Report} from "../model/Report.ts";
-import Day1 from "./Report/Day1.tsx";
-
 const TodayReportContainer = styled.div`
 `;
 
-const reportArr: Report[] = [
-    {
-        day: 1,
-        name: "신건우",
-        desc: "과제완료"
-    },
-];
 
 const TodayReport: React.FC = () => {
+
     return(
         <TodayReportContainer>
-            <ul>
-                {reportArr.map((report: Report) => (
-                    <li key={report.day}>
-                        <h2>{report.day} 일차 과제 : 웹사이트 뼈대 만들기!</h2>
-
-                        <h3><strong>이름</strong> : {report.name}</h3>
-
-                        <h3><strong>이메일</strong> : spacedustw@gmail.com</h3>
-
-                        <Day1 />
-                    </li>
-                ))}
-            </ul>
         </TodayReportContainer>
     )
 }
