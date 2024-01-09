17d82de9ab8ea823429febda4f814fb215996cbc
owen <wavefunctionc@gmail.com>
2024-01-09 17:06:54 +0900

    과제 2 완료

--- React/src/components/Report/Day2.tsx
+++ React/src/components/Report/Day2.tsx
@@ -1,36 +1,116 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
-// Define styled components
-const Day2Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+// 2-1
+const Container1 = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 const Box = styled.div`
-  border: 50px solid red;
-  padding: 50px;
-  width: 200px;
-  margin-bottom: 20px;
+    border: 50px solid lightgoldenrodyellow;
+    padding: 50px;
+    width: 200px;
+    margin-bottom: 20px;
 `;
 
 const GreenBox = styled(Box)`
-  border-color: green;
+    border-color: lightblue;
 `;
 
 const BlueBox = styled(Box)`
-  border-color: blue;
+    border-color: lightgreen;
 `;
+
+// 2-2
+const Container2 = styled.div`
+    background-color: #fff;
+    background-image: linear-gradient(90deg, transparent 79px, #abced4 79px, #abced4 81px, transparent 81px),
+    linear-gradient(#eee .1em, transparent .1em);
+    background-size: 100% 1.2em;
+    padding-left: 10%;
+`;
+
+const Paragraph = styled.p`
+    font-style: italic;
+    font-size: 16px;
+    font-family: Verdana;
+
+    &::first-letter {
+        font-size: 200%;
+        color: #2980b9;
+    }
+`;
+
+const SubHeader = styled.p`
+    color: #2980b9;
+`;
+
+const Title = styled.h1<{ firstChild?: boolean }>`
+    color: #9b59b6;
+
+    &::before {
+        content: "Topic: ";
+    }
+
+    ${props => props.firstChild && css`
+        text-align: center;
+    `}
+`;
+
+const HtmlStudy = [
+    "div : 영역을 나누는데 쓰임",
+    "p : 문단을 나누는데 쓰임 (div태그와 거의 비슷한 용도이나 단지 밑에 공간이 좀더 생김)",
+    "img : 이미지 넣는데 쓰임. 단일태그임",
+    "input : 검색창처럼 사용자에게 입력을 받는 태그 단일태그임",
+    "button : 버튼을 만듬",
+    "a : 링크를 연결해서 여러페이지로 이동할수있음"]
+
+const CssStudy = [
+    "color : 텍스트의 색을 바꿈",
+    "background-color : 배경색을 바꿈",
+    "border : 테두리를 그려줌, 3개의 인자를 받음 (두께, 스타일, 색깔)",
+    "width / height : 넓이와 높이를 지정하는데 쓰임",
+    "text-align : 정렬 (left, center, right)",
+    "margin : 내 영역 밖에 공간을 줌 (top, left, right, bottom)",
+    "padding : 내 영역 안에서 공간을 줌 (top, left, right, bottom)"]
 
 // Create the React component
 const Day2: React.FC = () => {
     return (
-        <Day2Container>
-            <Box>Box 1</Box>
-            <GreenBox>Box 2</GreenBox>
-            <BlueBox>Box 3</BlueBox>
-        </Day2Container>
+        <div>
+            <div>
+                <Container1>
+                    <Box>1번 박스</Box>
+                    <GreenBox>2번 박스</GreenBox>
+                    <BlueBox>3번 박스</BlueBox>
+                </Container1>
+            </div>
+
+            <div>
+                <Container2>
+                    <Title firstChild={true}>많이쓰는 HTML태그 정리! 📚</Title>
+                    <SubHeader>HTML은 웹 사이트의 뼈대를 그리는 마크업 언어 입니다.</SubHeader>
+                    <Paragraph>
+                        <ul>
+                            {HtmlStudy.map((item:string, index:number) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+                    </Paragraph>
+                    <Title>기본적인 CSS 정리! 📚</Title>
+                    <SubHeader>CSS는 뼈대를 갗춘 HTML에 디자인을 넣어줍니다.</SubHeader>
+                    <Paragraph>
+                        <ul>
+                            {CssStudy.map((item: string, index: number) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+                    </Paragraph>
+                </Container2>
+            </div>
+        </div>
     );
 };
 
--- React/src/components/TodayReport.tsx
+++ React/src/components/TodayReport.tsx
@@ -1,14 +1,72 @@
 import React from "react";
 import styled from "styled-components";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Day1 from "./Report/Day1.tsx";
+import Day2 from "./Report/Day2.tsx";
+
 const TodayReportContainer = styled.div`
+    text-align: center;
+    font-family: Arial, sans-serif;
+    color: #333;
+    background-color: #f9f9f9;
+    padding: 20px;
 `;
 
+const TitleDiv = styled.div`
+    text-align: center;
+    font-style: inherit;
+    font-size: 1.5em;
+    margin-bottom: 20px;
+`;
+
+const DayDiv = styled.div`
+    background-color: #abced4;
+    padding: 15px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+`;
+
+const AssignmentTitle = styled.h2`
+    margin: 0 0 10px 0;
+    font-size: 1.2em;
+`;
+
+const PFont = styled.p`
+    font-size: 19px;
+`;
+
+const Home = () => (
+    <TodayReportContainer>
+        <div>
+            <TitleDiv>
+                <h2><strong>신건우 과제 완료 목록</strong></h2>
+                <PFont><strong>이메일</strong> : spacedustw@gmail.com</PFont>
+            </TitleDiv>
+
+            <DayDiv>
+                <AssignmentTitle>1일차 과제</AssignmentTitle>
+                <Link to="/day-01-Assignment"><PFont>웹사이트 뼈대 만들기</PFont></Link>
+            </DayDiv>
+
+            <DayDiv>
+                <AssignmentTitle>2일차 과제</AssignmentTitle>
+                <Link to="/day-02-Assignment"><PFont>CSS를 연습하자</PFont></Link>
+            </DayDiv>
+        </div>
+    </TodayReportContainer>
+);
 
 const TodayReport: React.FC = () => {
 
-    return(
-        <TodayReportContainer>
-        </TodayReportContainer>
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Day-01-Assignment" element={<Day1/>}/>
+                <Route path="/Day-02-Assignment" element={<Day2/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
