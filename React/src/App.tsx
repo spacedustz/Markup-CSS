f947ed249ab274a316f5729d2289a2189934deed
KunWoo Shin <cyberspecterr@gmail.com>
2024-01-09 20:18:10 +0900

    로직 업데이트

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -3,23 +3,27 @@ import styled from "styled-components";
 import TodayReport from "./components/TodayReport.tsx";
 
 const AppBlock = styled.div`
-    width: 512px;
+    width: 900px;
     margin: 0 auto;
     margin-top: 4rem;
     border: 1px solid black;
     padding: 1rem;
 `;
 
+const RootStyle = styled.div`
+    background-image: url("./asset/space_small.png");
+`;
+
 const App: React.FC = () => {
 
     return (
-        <div>
+        <RootStyle>
             <div>
                 <AppBlock>
                     <TodayReport />
                 </AppBlock>
             </div>
-        </div>
+        </RootStyle>
     )
 }
 
