93c9238f4afdfdd2acdc70d5182ec4b043ad4ef3
spacedustz <tensorflow555@gmail.com>
2024-01-09 20:18:10 +0900

    로직 업데이트

--- React/src/components/TodayReport.tsx
+++ React/src/components/TodayReport.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import Day1 from "./Report/Day1.tsx";
 import Day2 from "./Report/Day2.tsx";
+import CommentApp from "./Comment/CommentApp.tsx";
 
 const TodayReportContainer = styled.div`
     text-align: center;
@@ -22,8 +23,17 @@ const TitleDiv = styled.div`
 const DayDiv = styled.div`
     background-color: #abced4;
     padding: 15px;
-    margin-bottom: 10px;
+    margin-bottom: 20px;
     border-radius: 5px;
+`;
+
+const CommentDiv = styled.div`
+    padding: 2px;
+    margin-top: 60px;
+    margin-bottom: 20px;
+    border-radius: 5px;
+    border: 1px solid black;
+    padding-bottom: 50px;
 `;
 
 const AssignmentTitle = styled.h2`
@@ -35,23 +45,38 @@ const PFont = styled.p`
     font-size: 19px;
 `;
 
+const CommentTitle = styled.h2`
+    margin: 0 0 10px 0;
+    margin-top: 30px;
+    font-size: 1.2em;
+`;
+
 const Home = () => (
     <TodayReportContainer>
         <div>
             <TitleDiv>
-                <h2><strong>신건우 과제 완료 목록</strong></h2>
+                <h2><strong>📚 과제 완료 목록📚 </strong></h2>
+                <PFont><strong>이름</strong> : 신건우</PFont>
                 <PFont><strong>이메일</strong> : spacedustw@gmail.com</PFont>
             </TitleDiv>
 
             <DayDiv>
-                <AssignmentTitle>1일차 과제</AssignmentTitle>
+                <AssignmentTitle>🚩 1일차 과제</AssignmentTitle>
                 <Link to="/day-01-Assignment"><PFont>웹사이트 뼈대 만들기</PFont></Link>
             </DayDiv>
 
             <DayDiv>
-                <AssignmentTitle>2일차 과제</AssignmentTitle>
+                <AssignmentTitle>🚩 2일차 과제</AssignmentTitle>
                 <Link to="/day-02-Assignment"><PFont>CSS를 연습하자</PFont></Link>
             </DayDiv>
+        </div>
+
+        <div>
+            <CommentDiv>
+                <CommentTitle>
+                    <CommentApp/>
+                </CommentTitle>
+            </CommentDiv>
         </div>
     </TodayReportContainer>
 );
@@ -62,7 +87,7 @@ const TodayReport: React.FC = () => {
 
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home/>}/>
                 <Route path="/Day-01-Assignment" element={<Day1/>}/>
                 <Route path="/Day-02-Assignment" element={<Day2/>}/>
             </Routes>
