3ab5de571ade40fba7345f1a28d5ae426611c868
Kunwoo Shin <recyclefunction@gmail.com>
2024-01-09 22:23:25 +0900

    로직 업데이트

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -5,25 +5,17 @@ import TodayReport from "./components/TodayReport.tsx";
 const AppBlock = styled.div`
     width: 900px;
     margin: 0 auto;
-    margin-top: 4rem;
+    margin-top: 3rem;
     border: 1px solid black;
     padding: 1rem;
-`;
-
-const RootStyle = styled.div`
-    background-image: url("./asset/space_small.png");
 `;
 
 const App: React.FC = () => {
 
     return (
-        <RootStyle>
-            <div>
-                <AppBlock>
-                    <TodayReport />
-                </AppBlock>
-            </div>
-        </RootStyle>
+        <AppBlock>
+            <TodayReport/>
+        </AppBlock>
     )
 }
 
