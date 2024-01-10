a64e41abdeeab0cc7d9565480b405ca74a0100b2
Kunwoo Shin <recyclefunction@gmail.com>
2024-01-11 00:38:38 +0900

    백엔드 서버 추가 및 댓글 로직 수정

---
+++ API-Server/src/main/java/com/apiserver/CommentDto.java
@@ -0,0 +1,9 @@
+package com.apiserver;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CommentDto {
+    private String comment;
+}
--- React/src/components/Comment/CommentList.tsx
+++ React/src/components/Comment/CommentList.tsx
@@ -1,15 +1,55 @@
-import React from "react";
+// CommentList.tsx
+import React from 'react';
 import {CommentListProps} from "../../model/Comment.ts";
+import styled from "styled-components";
+
+const CommentStyle = styled.div`
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 20px;
+    }
+
+    th, td {
+        border: 1px solid #ddd;
+        padding: 12px;
+        text-align: left;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    td:nth-child(2) { /* Comment column */
+        width: 45%;
+    }
+
+    td:nth-child(3) { /* CreatedAt column */
+        width: 20%;
+    }
+`;
 
 const CommentList: React.FC<CommentListProps> = ({ comments }) => (
-    <div>
-        <h2>피드백 목록</h2>
-        <ul>
+    <CommentStyle>
+        <table>
+            <thead>
+            <tr>
+                <th>댓글번호</th>
+                <th>댓글</th>
+                <th>작성일자</th>
+            </tr>
+            </thead>
+            <tbody>
             {comments.map((comment, index) => (
-                <li key={index}>{comment}</li>
+                <tr key={index}>
+                    <td>{comment.id}</td>
+                    <td>{comment.comment}</td>
+                    <td>{comment.createdAt}</td>
+                </tr>
             ))}
-        </ul>
-    </div>
+            </tbody>
+        </table>
+    </CommentStyle>
 );
 
 export default CommentList;
