c881e342a449ba9af0cfa4027278574c6601dbda
KunWoo Shin <cyberspecterr@gmail.com>
2024-01-11 00:38:38 +0900

    백엔드 서버 추가 및 댓글 로직 수정

--- React/src/components/Comment/CommentApp.tsx
+++ React/src/components/Comment/CommentApp.tsx
@@ -1,31 +1,49 @@
-import React, {useEffect, useState} from "react";
-import CommentList from "./CommentList.tsx";
+import React, { useState, useEffect } from 'react';
 import CommentForm from "./CommentForm.tsx";
+import CommentList from "./CommentList.tsx";
+import axios from 'axios';
+import {Comment} from "../../model/Comment.ts";
 
 const CommentApp: React.FC = () => {
-    const [comments, setComments] = useState<string[]>([]);
+    const [comments, setComments] = useState<Comment[]>([]);
 
     useEffect(() => {
-        // 로컬 스토리지에서 댓글 데이터 불러오기
-        const storedComments = localStorage.getItem('comments');
-        if (storedComments) {
-            setComments(JSON.parse(storedComments));
-        }
+        // Fetch comments from the backend every 3 seconds
+        const fetchComments = async () => {
+            try {
+                const response = await axios.get<Comment[]>('http://13.125.251.209:8080/api/comments');
+                // const response = await axios.get<Comment[]>('http://localhost:8080/api/comments');
+                setComments(response.data);
+            } catch (error) {
+                console.error('Error fetching comments:', error);
+            }
+        };
+
+        const intervalId = setInterval(fetchComments, 500);
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
     }, []);
 
-    useEffect(() => {
-        // 댓글 데이터가 변경될 때마다 로컬 스토리지에 저장
-        localStorage.setItem('comments', JSON.stringify(comments));
-    }, [comments]);
+    const handleAddComment = async (comment: string) => {
+        try {
+            // Send the new comment to the backend
+            const response = await axios.post('http://13.125.251.209:8080/api/comments', { comment });
+            // const response = await axios.post('http://localhost:8080/api/comments', { comment: comment });
 
-    const handleAddComment = (comment: string) => {
-        setComments([...comments, comment]);
+            const newComment = response.data;
+
+            // Update the local state with the new comment
+            setComments([...comments, newComment]);
+        } catch (error) {
+            console.error('Error adding comment:', error);
+        }
     };
 
     return (
         <div>
             <CommentForm onAddComment={handleAddComment} />
-            <CommentList comments={comments} />
+            <CommentList comments={comments}/>
         </div>
     );
 };
