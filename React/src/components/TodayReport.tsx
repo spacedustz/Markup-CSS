53acfa43aef63541233f234e6951efcbbba5a43c
KunWoo Shin <cyberspecterr@gmail.com>
2024-01-13 01:37:47 +0900

    Styled Container에 Alpha 값 추가

--- React/src/components/TodayReport.tsx
+++ React/src/components/TodayReport.tsx
@@ -21,7 +21,7 @@ const StyledContainer = styled.div`
     text-align: center;
     font-family: Arial, sans-serif;
     color: #333;
-    background-color: #f9f9f9;
+    background-color: rgba(249, 249, 249, 0.5);
     padding: 20px;
     max-width: 600px; /* 필요에 따라 조절 */
     border: 1px solid black;
