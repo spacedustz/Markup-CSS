1c4b29eb2ff5bf115219546a536331b244889f37
KunWoo Shin <cyberspecterr@gmail.com>
2024-01-12 23:42:15 +0900

    댓글 Pagination 테스트

--- React/src/components/Comment/Pagination.tsx
+++ React/src/components/Comment/Pagination.tsx
@@ -1,5 +1,32 @@
 import React from "react";
 import {PaginationProps} from "../../model/Comment.ts";
+import styled from "styled-components";
+
+const PaginationContainer = styled.div`
+    ul {
+        list-style: none;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    li {
+        margin-right: 10px;
+    }
+
+    button {
+        background-color: #3498db;
+        color: #fff;
+        border: 1px solid #3498db;
+        padding: 8px 12px;
+        cursor: pointer;
+
+        &:hover {
+            background-color: #217dbb;
+        }
+    }
+`;
 
 const Pagination: React.FC<PaginationProps> = ({itemsPerPage, totalItems, currentPage, paginate }) => {
     const pageNumbers = [];
@@ -9,7 +36,7 @@ const Pagination: React.FC<PaginationProps> = ({itemsPerPage, totalItems, curren
     }
 
     return (
-        <div>
+        <PaginationContainer>
             <ul>
                 {pageNumbers.map(number => (
                     <li key={number}>
@@ -17,7 +44,7 @@ const Pagination: React.FC<PaginationProps> = ({itemsPerPage, totalItems, curren
                     </li>
                 ))}
             </ul>
-        </div>
+        </PaginationContainer>
     );
 }
 
