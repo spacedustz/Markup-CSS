b1f1320a237134590f72567c3cb16052f5f4f919
spacedustz <tensorflow555@gmail.com>
2024-01-13 15:50:55 +0900

    댓글 삭제기능 구현

--- API-Server/src/main/java/com/apiserver/CommentDto.java
+++ API-Server/src/main/java/com/apiserver/CommentDto.java
@@ -6,4 +6,9 @@ import lombok.Setter;
 @Getter @Setter
 public class CommentDto {
     private String comment;
+
+    @Getter
+    public static class Delete {
+        private int commentId;
+    }
 }
