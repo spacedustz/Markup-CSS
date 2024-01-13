7184b09e0fd52390ebafbed959d06c6f1e6cbeb8
spacedustz <tensorflow555@gmail.com>
2024-01-13 11:39:44 +0900

    Day6 중간 커밋

--- React/src/components/Report/Day6.tsx
+++ React/src/components/Report/Day6.tsx
@@ -3,19 +3,20 @@ import styled from "styled-components";
 
 const MainContainer = styled.div`
     display: flex;
-    background-color: #2a2a2a;
     border: 1px solid red;
     height: 100vh;
     width: 100%;
 `;
 
 const LeftContainer = styled.div`
+    background-color: black;
     display: flex;
     flex-direction: column;
     width: 30%;
 `;
 
 const RightContainer = styled.div`
+    background-color: black;
     display: flex;
     flex-direction: column;
     width: 70%;
@@ -23,14 +24,34 @@ const RightContainer = styled.div`
 
 /* --- */
 const LeftLogo = styled.div`
+    background-color: #2a2a2a;
     display: flex;
     flex-direction: column;
+    border-radius: 5px;
+    margin: 4px;
+    height: 20%;
     
     img {
         height: 24px;
         width: 24px;
     }
 `;
+
+const LeftLibrary = styled.div`
+    background-color: #2a2a2a;
+    display: flex;
+    flex-direction: column;
+    height: 20%;
+    margin: 4px;
+`;
+
+const LeftFooter = styled.div`
+    background-color: #2a2a2a;
+    display: flex;
+    flex-direction: column;
+    height: 60%;
+    margin: 4px;
+`
 
 const Day6: React.FC = () => {
 
@@ -42,8 +63,14 @@ const Day6: React.FC = () => {
                     <a href="#">검색하기</a>
                     <img src="../../../public/assets/Spotify/search.svg"/>
                 </LeftLogo>
-                <div></div>
-                <div></div>
+
+                <LeftLibrary>
+                    Left Middle
+                </LeftLibrary>
+
+                <LeftFooter>
+                    Left Footer
+                </LeftFooter>
             </LeftContainer>
 
             <RightContainer>
