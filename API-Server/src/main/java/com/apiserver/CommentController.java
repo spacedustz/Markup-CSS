87531c6076180c3ab7f31c10523acd9b0970ae46
KunWoo Shin <cyberspecterr@gmail.com>
2024-01-13 15:50:55 +0900

    댓글 삭제기능 구현

--- API-Server/src/main/java/com/apiserver/CommentController.java
+++ API-Server/src/main/java/com/apiserver/CommentController.java
@@ -23,7 +23,11 @@ public class CommentController {
     @PostMapping
     public ResponseEntity<String> addComment(@RequestBody CommentDto comment) {
         commentService.saveComment(comment);
-        // 여기에 추가로 로직을 넣을 수 있습니다 (예: 데이터베이스에 저장).
         return ResponseEntity.ok("댓글이 성공적으로 추가 되었습니다");
+    }
+
+    @DeleteMapping
+    public void deleteComment(@RequestBody CommentDto.Delete dto) {
+        commentService.deleteOne(dto);
     }
 }
