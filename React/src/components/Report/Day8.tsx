7c0e2ef4c6a21cee4ae523dbe9210d76dd83ceb3
owen <floralexium@gmail.com>
2024-01-15 12:50:09 +0900

    BootStrap Module 추가, Add to main.tsx

---
+++ React/src/components/Report/Day8.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Button} from "react-bootstrap";
+
+const Day8:React.FC = () => {
+    return (
+        <div>
+            <Button variant="primary">Primary Button</Button>
+        </div>
+    )
+}
+
+export default Day8;
