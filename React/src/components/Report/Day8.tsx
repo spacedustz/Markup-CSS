714bfeaad52b37b37c66ca2c96f368e9edb4b8d7
owen <wavefunctionc@gmail.com>
2024-01-16 14:02:09 +0900

    중복 컨테이너 제거

--- React/src/components/Report/Day8.tsx
+++ React/src/components/Report/Day8.tsx
@@ -3,8 +3,14 @@ import {Button} from "react-bootstrap";
 
 const Day8:React.FC = () => {
     return (
-        <div>
-            <Button variant="primary">Primary Button</Button>
+        <div className="row">
+            <Button variant="primary" className="col-2">Primary Button</Button>
+
+            <Button variant="primary" className="col-2">Primary Button</Button>
+
+            <Button variant="primary" className="col-2">Primary Button</Button>
+
+            <Button variant="primary" className="col-2">Primary Button</Button>
         </div>
     )
 }
