0f5c62aa4448d1c70cb93d8a7b139cd52c7fad2c
spacedustz <spacedustw@gmail.com>
2024-01-16 14:02:09 +0900

    중복 컨테이너 제거

--- React/src/components/TodayReport.tsx
+++ React/src/components/TodayReport.tsx
@@ -136,14 +136,8 @@ const Home = () => (
                     </DayDiv>
 
                     <DayDiv>
-                        <AssignmentTitle>🚩 6번째 과제</AssignmentTitle>
-                        <AssignmentFont to="/Day-06-Assignment"><PFont>Spotify 최신 페이지 (왼쪽부분 미완성)</PFont></AssignmentFont>
-                        <AssignmentFont to="/Day-06-02-Assignment"><PFont>Spotify 예전 페이지 (진행중)</PFont></AssignmentFont>
-                    </DayDiv>
-
-                    <DayDiv>
                         <AssignmentTitle>🚩 7번째 과제</AssignmentTitle>
-                        <AssignmentFont to="/Day-08-Assignment"><PFont>BootStrap</PFont></AssignmentFont>
+                        <AssignmentFont to="/Day-08-Assignment"><PFont>BootStrap을 이용해 사이트 만들기</PFont></AssignmentFont>
                     </DayDiv>
                 </StyledContainer>
             </div>
